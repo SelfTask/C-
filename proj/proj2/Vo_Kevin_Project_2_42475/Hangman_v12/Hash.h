3b167b2fd569a58449bdc37ea05a9fb7f5ec58d9
SelfTask <selftasker@gmail.com>
2018-05-31 21:36:02 -0700

    start new

---
+++ proj/proj2/Vo_Kevin_Project_2_42475/Hangman_v12/Hash.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Hash.h
+ * Author: DangerousPirate
+ *
+ * Created on May 31, 2018, 9:34 PM
+ */
+
+#ifndef HASH_H
+#define HASH_H
+
+
+
+#endif /* HASH_H */
+
